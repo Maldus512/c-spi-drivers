6905605bc17287b15397d6bfaf83192dc5831d3c
Virginia <virginia.negri@studio.unibo.it>
2021-12-02 16:30:16 +0100

    modulo adc mcp3202

---
+++ spi_devices/adc/MCP3202/mcp3202.h
@@ -0,0 +1,19 @@
+#ifndef MCP3202_H_INCLUDED
+#define MCP3202_H_INCLUDED
+
+#include <stdint.h>
+#include "spi_common/spi_common.h"
+
+
+typedef enum
+{
+    SPI_ADC_CH1 = 1,
+    SPI_ADC_CH2,
+} adc_spi_channel_t;
+
+int mcp3202_read_adc_channel(spi_driver_t driver, adc_spi_channel_t ch, uint16_t *value);
+
+#endif
+
+
+
