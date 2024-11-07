53bd8c86ba89463a2a62ed8b00ddf272e90962de
Maldus512 <mattia512maldini@gmail.com>
2024-11-07 17:33:48 +0100

    MCP3202 build system integration

--- spi_devices/adc/MCP3202/mcp3202.c
+++ spi_devices/adc/MCP3202/mcp3202.c
@@ -1,11 +1,11 @@
 #include "mcp3202.h"
 #include "spi_common/spi_common.h"
 
+
 int mcp3202_read_adc_channel(spi_driver_t driver, adc_spi_channel_t ch, uint16_t *value) {
-    
-    uint8_t request = 0;
-    uint8_t buffer[3] = {0};
-    uint8_t read_buffer[3] =  {0};
+    uint8_t  request        = 0;
+    uint8_t  buffer[3]      = {0};
+    uint8_t  read_buffer[3] = {0};
     uint16_t reading;
 
     if (ch != SPI_ADC_CH1 && ch != SPI_ADC_CH2)
@@ -22,8 +22,8 @@ int mcp3202_read_adc_channel(spi_driver_t driver, adc_spi_channel_t ch, uint16_t
     driver.spi_exchange(buffer, read_buffer, 3, driver.user_data);
 
     reading = ((read_buffer[0] & 0x7) << 9) | (read_buffer[1] << 1) | ((read_buffer[2] & 0x80) >> 7);
-    *value=reading;
-    
+    *value  = reading;
+
     SPI_CS(driver, 1);
     return 0;
 }
--- spi_devices/adc/MCP3202/mcp3202.h
+++ spi_devices/adc/MCP3202/mcp3202.h
@@ -5,15 +5,13 @@
 #include "spi_common/spi_common.h"
 
 
-typedef enum
-{
+typedef enum {
     SPI_ADC_CH1 = 1,
     SPI_ADC_CH2,
 } adc_spi_channel_t;
 
+
 int mcp3202_read_adc_channel(spi_driver_t driver, adc_spi_channel_t ch, uint16_t *value);
 
+
 #endif
-
-
-
